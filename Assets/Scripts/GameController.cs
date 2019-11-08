8081b002c1fad0e7efb5b408d6588ce11888a57e
dminhpro123 <nguyenvducminh@gmail.com>
2019-11-09 00:31:03 +0700

    First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public float maxEstimate = 100f;
+    public float curEstimate;
+
+    Text textEstimate;
+    float timeDelay;
+    float nextTime;
+    Estimate estimate;
+    int estimates;
+    public GameObject textEstimateComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textEstimate = textEstimateComponent.GetComponent<Text>();
+        textEstimate.text = ""+maxEstimate;
+        curEstimate = maxEstimate;
+        timeDelay = 1f;
+        nextTime = Time.time;
+    }
+
+    
+    
+    void FixedUpdate()
+    {
+        checkTime();
+    }
+
+    void checkTime()
+    {
+        if(Time.time > nextTime)
+        {
+            curEstimate--;
+            nextTime = Time.time + timeDelay;
+            textEstimate.text = "" + curEstimate;
+        }
+    }
+}
---
+++ Assets/Scripts/Quantity/HpUi.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpUi : MonoBehaviour
+{
+    public GameController player;
+    // Start is called before the first frame update
+    Vector3 localScale;
+    Vector3 localPosition;
+
+    Vector3 StartScale;
+    Vector3 StartPosition;
+
+    float newScaleX;
+    float newPosX;
+    float hpDe;// mau giam
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        StartScale = transform.localScale;
+        StartPosition = GameObject.FindWithTag("HpUI").transform.localPosition;
+    }
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (player.curEstimate <= 0)
+        {
+            KillSelf();
+        }
+        else
+        {
+            hpDe = (player.maxEstimate - player.curEstimate) / player.maxEstimate;
+
+            newScaleX = (StartScale.x) - (StartScale.x) * hpDe;
+
+            newPosX = StartPosition.x - (StartScale.x - newScaleX) / 2;
+
+            localScale = StartScale;
+            localScale.x = newScaleX; /*Scale hien tai*/
+            localPosition = StartPosition;
+            localPosition.x = newPosX;
+            transform.localScale = localScale;
+            transform.localPosition = localPosition;
+
+        }
+
+
+    }
+    private void KillSelf()
+    {
+        Destroy(this.gameObject);
+    }
+}
